fcb25c9fd3b30fcf4420cf25a24c92f53057d272
Edison Morales Alonzo <arqema62@MacBook-Air-de-Edison.local>
2023-02-16 09:47:32 -0600

    first one

---
+++ testing.cs
@@ -0,0 +1,11 @@
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine("what the hell"");
+        for (int i = 0; i < mejor; i++){
+            Jesus();
+        }
+    }
+}
